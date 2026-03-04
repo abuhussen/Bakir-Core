deb3ff0f84311be9b17dd7c61807098614b78146
abuhussen <abuhussenbakir@gmail.com>
2026-03-04 10:23:29 +0300

    💎 Restoration: Recovered Theme and Aliases to the project core

---
+++ bakir-configs/bakir-aliases.sh
@@ -0,0 +1,6 @@
+# اختصارات باكير السيادية
+alias b-store='bakir-store'
+alias b-sys='bakir-sys'
+alias b-shield='bakir-shield'
+alias b-killer='bakir-killer'
+alias update='sudo apt update && sudo apt upgrade -y'
